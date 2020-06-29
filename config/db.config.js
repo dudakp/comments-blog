cab39462f36365760955d11645235c0191ab801a
Pavol Ďuďák <palodudak@gmail.com>
2020-06-29 10:03:39 +0200

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  // MONGO_URL: `mongodb+srv://admin:M2jt6vSDS0@blog-cluster-fpcxs.gcp.mongodb.net/blog-comments?retryWrites=true&w=majority`
   MONGO_URL: process.env.MONGO_URL,
 };
